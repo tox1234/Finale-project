98a23264dfa78b3e27490734d32c584bf1d7f673
tox1234 <102309965+tox1234@users.noreply.github.com>
2025-04-07 11:51:12 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,47 +1,79 @@
 import socket
 
-
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 5001
-
-
-action = input("Choose action - UPLOAD or DOWNLOAD: ").strip().upper()
-
-if action not in ["UPLOAD", "DOWNLOAD"]:
-    print("Invalid action. Please choose UPLOAD or DOWNLOAD.")
-    exit()
+HOST = '127.0.0.1'
+PORT = 5000
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_HOST, SERVER_PORT))
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    print(client.recv(1024).decode(), end='')
+    action = input()
+    client.sendall(action.encode())
+
+    if action.lower().strip() == 'register':
+        print(client.recv(1024).decode(), end='')
+        username = input()
+        client.sendall(username.encode())
+
+        print(client.recv(1024).decode(), end='')
+        password = input()
+        client.sendall(password.encode())
+
+        print(client.recv(1024).decode())
+        client.close()
+        return
+
+    print(client.recv(1024).decode(), end='')
+    username = input()
+    client.sendall(username.encode())
+
+    print(client.recv(1024).decode(), end='')
+    password = input()
+    client.sendall(password.encode())
+
+    response = client.recv(1024).decode()
+    print(response)
+
+    if "Login successful" in response:
+        while True:
+            print(client.recv(1024).decode(), end='')
+            option = input()
+            client.sendall(option.encode())
+
+            if option == 'upload':
+                print(client.recv(1024).decode(), end='')
+                filename = input()
+                client.sendall(filename.encode())
+
+                try:
+                    with open(filename, 'rb') as f:
+                        data = f.read()
+                    client.sendall(data)
+                    print(client.recv(1024).decode())
+                except FileNotFoundError:
+                    print("File not found!")
+
+            elif option == 'download':
+                print(client.recv(1024).decode(), end='')
+                filename = input()
+                client.sendall(filename.encode())
+
+                data = client.recv(1024)
+                if b'File not found' in data:
+                    print(data.decode())
+                else:
+                    with open(filename, 'wb') as f:
+                        f.write(data)
+                    print("File downloaded successfully!")
+
+            elif option == 'exit':
+                print(client.recv(1024).decode())
+                break
+
+    client.close()
 
 
-client_socket.send(action.encode())
-
-if action == "DOWNLOAD":
-    file_name = input("Enter the name of the file to download: ")
-    client_socket.send(file_name.encode())
-
-    response = client_socket.recv(1024).decode()
-
-    if response == "FOUND":
-        with open(file_name, "wb") as file:
-            while chunk := client_socket.recv(1024):
-                file.write(chunk)
-        print(f"File '{file_name}' downloaded successfully.")
-    else:
-        print(f"File '{file_name}' not found on server.")
-
-elif action == "UPLOAD":
-    file_name = input("Enter the name of the file to upload: ")
-    try:
-        with open(file_name, "rb") as file:
-            client_socket.send(file_name.encode())
-            while chunk := file.read(1024):
-                client_socket.send(chunk)
-            client_socket.send(b"END")
-        print(f"File '{file_name}' uploaded successfully.")
-    except FileNotFoundError:
-        print(f"File '{file_name}' not found on client.")
-
-client_socket.close()
+if __name__ == "__main__":
+    main()
